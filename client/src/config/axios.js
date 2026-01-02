e863e16a2f968d0e24f2cc45cf3489054ce38c37
Vishwas Chourasiya <vishwaschourasiya@gmail.com>
2026-01-03 01:43:49 +0530

    axios

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : 'https://backend-dkh2.onrender.com/api',
     withCredentials: true
 });
 

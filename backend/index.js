637d73abe86986c1f6b254c02f54186d2542ba4c
Vishwas Chourasiya <vishwaschourasiya@gmail.com>
2026-01-08 21:30:57 +0530

    Fix CORS and add MongoDB driver

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://frontend-qqtw.onrender.com'],
     credentials: true
 }));
 app.use(cookieParser());
